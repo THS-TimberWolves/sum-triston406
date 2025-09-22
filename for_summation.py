0e79650ba8d054a95f8ad13f8748aed69df01077
triston406 <robertri020@alpinesd.org>
2025-09-22 11:29:47 -0600

    Update for_summation.py

--- for_summation.py
+++ for_summation.py
@@ -1,1 +1,8 @@
 #for Summation code here
+def for_summation(x):
+    total = 0
+    for x in range(0,x+1,1):
+        total += x
+    return total 
+x = int(input("input number"))
+print(for_summation(x))
