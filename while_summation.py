7bc06983deb9a4314e6e558e6e8d85ece759004d
triston406 <robertri020@alpinesd.org>
2025-09-22 11:12:12 -0600

    Update while_summation.py

--- while_summation.py
+++ while_summation.py
@@ -1,1 +1,15 @@
 #while summation code following directions from Readme
+def while_summation(summation):
+    total = 1
+    num = 1
+    while  num < summation:
+        num += 1
+        total += num
+    return total 
+    
+
+summation = int((input()))
+
+
+
+print((while_summation(summation)))
